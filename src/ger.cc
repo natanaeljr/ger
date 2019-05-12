a1d808023e23d43ffea1e13592a1bd92ed58f964
Natanael <natanael.rabello@outlook.com>
2019-05-12 19:39:16 -0300

    Appeal to Conan for dependency manager

--- src/ger.cc
+++ src/ger.cc
@@ -1,13 +1,14 @@
-#include <libssh/libssh.h>
 #include "gsl/gsl"
 #include "fmt/core.h"
 #include "absl/strings/str_format.h"
+#include "libssh2.h"
 
 namespace ger {
 
 int ger(int argc, const char* argv[])
 {
-    ssh_new();
+    std::string s = absl::StrFormat("%s", "Hi");
+    libssh2_init(0);
     gsl::finally([]{});
     fmt::print("Ger!\n");
     return 0;
