9ac946a6e30f962d91df9c92588109f0f94ce21b
Natanael Rabello <natanael.rabello@outlook.com>
2019-11-25 09:43:18 -0300

    Fix curl digest auth

--- src/gerlib/lib.rs
+++ src/gerlib/lib.rs
@@ -48,30 +48,28 @@ impl Gerrit {
     // Perform a request to the REST API through libcurl
     pub fn request_json<S: AsRef<str>>(&self, query_str: S) -> Result<String> {
         let mut easy = curl::easy::Easy::new();
-        easy.url(
-            format!(
-                "{}{}",
-                "https://gerrit-review.googlesource.com/a/",
-                query_str.as_ref()
-            )
-            .as_str(),
-        )?;
-        easy.username(self.username.as_str())?;
-        easy.password(self.password.as_str())?;
+        easy.url(format!("{}/a/{}", self.host, query_str.as_ref()).as_str())
+            .unwrap();
+        easy.username(self.username.as_str()).unwrap();
+        easy.password(self.password.as_str()).unwrap();
+        easy.http_auth(curl::easy::Auth::new().digest(true))
+            .unwrap();
+        easy.ssl_verify_peer(false).unwrap();
+        easy.ssl_verify_host(false).unwrap();
         let mut headers = curl::easy::List::new();
-        headers.append("Accept: application/json")?;
-        easy.http_headers(headers)?;
+        headers.append("Accept: application/json").unwrap();
+        easy.http_headers(headers).unwrap();
         let mut data: Vec<u8> = Vec::new();
         {
             let mut transfer = easy.transfer();
             transfer.write_function(|input| {
                 data.extend_from_slice(input);
                 Ok(input.len())
-            })?;
-            transfer.perform()?
+            }).unwrap();
+            transfer.perform().unwrap();
         }
         let response = String::from_utf8_lossy(data.as_slice()).to_string();
-        let res_code = easy.response_code()?;
+        let res_code = easy.response_code().unwrap();
         if res_code != 200 {
             return Err(Error::Generic(response));
         }
