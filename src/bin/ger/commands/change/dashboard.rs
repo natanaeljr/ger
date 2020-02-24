7eafb45af9a1929c64728db3c5b88477063b0cbf
Natanael Rabello <natanael.rabello@outlook.com>
2020-02-23 22:22:23 -0300

    Add ger change dashboard
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

---
+++ src/bin/ger/commands/change/dashboard.rs
@@ -0,0 +1,62 @@
+use super::list;
+use crate::config::{CliConfig, Verbosity};
+use crate::handler::get_remote_restapi_handler;
+use clap::{App, Arg, ArgMatches, SubCommand};
+use gerlib::changes::ChangeInfo;
+use http::uri::PathAndQuery;
+use log::info;
+use std::io::Write;
+use termcolor::{ColorSpec, WriteColor};
+
+/// Build the CLI
+pub fn cli() -> App<'static, 'static> {
+    SubCommand::with_name("dashboard")
+        .visible_alias("db")
+        .about(
+            "User's change dashboard.\n\
+             It is the default subcommand when none specified.",
+        )
+        .arg(
+            Arg::with_name("remote")
+                .long("remote")
+                .short("r")
+                .takes_value(true)
+                .value_name("NAME")
+                .help("Specify an alternative remote to use."),
+        )
+        .template("{about}\n\nUSAGE:\n    {usage}\n\n{all-args}")
+}
+
+/// Execute the command
+pub fn exec(config: &mut CliConfig, args: Option<&ArgMatches>) -> Result<(), failure::Error> {
+    let args = args.unwrap();
+    let verbose: Verbosity = args.occurrences_of("verbose").into();
+    let remote = args.value_of("remote");
+
+    let mut rest = get_remote_restapi_handler(config, remote)?;
+
+    let uri: PathAndQuery =
+        "/a/changes/?q=is:open+owner:self&q=is:open+reviewer:self+-owner:self&q=is:closed+owner:self+limit:10&o=DETAILED_ACCOUNTS&o=CURRENT_REVISION".to_string()
+        .parse()?;
+
+    info!("uri: {}", uri);
+    let json = rest.request_json(uri, verbose >= Verbosity::Debug)?;
+    let changes_vec: Vec<Vec<ChangeInfo>> = serde_json::from_str(json.as_str())?;
+
+    config.stdout.set_color(ColorSpec::new().set_italic(true))?;
+    writeln!(config.stdout, "* Outgoing reviews:")?;
+    config.stdout.reset()?;
+    list::list(config, &changes_vec[0])?;
+
+    config.stdout.set_color(ColorSpec::new().set_italic(true))?;
+    writeln!(config.stdout, "\n* Incoming reviews:")?;
+    config.stdout.reset()?;
+    list::list(config, &changes_vec[1])?;
+
+    config.stdout.set_color(ColorSpec::new().set_italic(true))?;
+    writeln!(config.stdout, "\n* Recently closed:")?;
+    config.stdout.reset()?;
+    list::list(config, &changes_vec[2])?;
+
+    Ok(())
+}
--- src/bin/ger/commands/change/mod.rs
+++ src/bin/ger/commands/change/mod.rs
@@ -1,6 +1,7 @@
 use crate::config::CliConfig;
-use clap::{App, AppSettings, ArgMatches, SubCommand};
+use clap::{App, ArgMatches, SubCommand};
 
+mod dashboard;
 mod list;
 mod show;
 
@@ -8,15 +9,18 @@ mod show;
 pub fn cli() -> App<'static, 'static> {
     SubCommand::with_name("change")
         .about("Lists changes and information about changes.")
-        .setting(AppSettings::ArgRequiredElseHelp)
         .template("{about}\n\nUSAGE:\n    {usage}\n\n{all-args}")
-        .subcommands(vec![list::cli(), show::cli()])
+        .subcommands(vec![dashboard::cli(), list::cli(), show::cli()])
 }
 
 /// Execute the command
 pub fn exec(config: &mut CliConfig, args: Option<&ArgMatches>) -> Result<(), failure::Error> {
     let args = args.unwrap();
     match args.subcommand() {
+        ("dashboard", subargs) | ("", subargs) => {
+            let def_args = ArgMatches::default();
+            dashboard::exec(config, subargs.or(Some(&def_args)))
+        }
         ("list", subargs) => list::exec(config, subargs),
         ("show", subargs) => show::exec(config, subargs),
         _ => Ok(()),
