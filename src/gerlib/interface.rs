a5985f901ebac40bd972614489e7ee7a712d33a1
Natanael Rabello <natanael.rabello@outlook.com>
2020-03-01 21:02:13 -0300

    WIP: GerritRestApi: filled topic releted functions
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- src/gerlib/interface.rs
+++ src/gerlib/interface.rs
@@ -5,21 +5,35 @@ use log::error;
 
 type Result<T> = std::result::Result<T, crate::error::Error>;
 
-struct GerritRestApi {
-    http_handler: HttpRequestHandler,
+pub struct GerritRestApi {
+    http: HttpRequestHandler,
 }
 
 impl GerritRestApi {
     pub fn get_topic(&mut self, change_id: &str) -> Result<String> {
         let url = format!("/a/changes/{}/topic", change_id);
-        let (code, response) = self.http_handler.get(&url)?;
-        Self::expect_http_status_code(200, code)?;
-        Self::json_strip_magic_prefix(response)
+        let (code, response) = self.http.get(&url)?;
+        Self::expect_response_code(200, code)?;
+        Self::strip_json_magic_prefix(response)
     }
-    pub fn set_topic(&self, change_id: &str, topic: TopicInput) {}
-    pub fn delete_topic(&self, change_id: &str) {}
 
-    fn expect_http_status_code(expected: u32, actual: u32) -> Result<()> {
+    pub fn set_topic(&mut self, change_id: &str, topic: TopicInput) -> Result<String> {
+        let url = format!("/a/changes/{}/topic", change_id);
+        let body = serde_json::to_string(&topic)?;
+        let (code, response) = self.http.put(&url, body.as_bytes())?;
+        Self::expect_response_code(200, code)?;
+        Self::strip_json_magic_prefix(response)?;
+        let topic: String = serde_json::from_str(&response)?;
+        Ok(topic)
+    }
+
+    pub fn delete_topic(&mut self, change_id: &str) -> Result<()> {
+        let url = format!("/a/changes/{}/topic", change_id);
+        let (code, response) = self.http.delete(&url)?;
+        Self::expect_response_code(204, code)
+    }
+
+    fn expect_response_code(expected: u32, actual: u32) -> Result<()> {
         if expected != actual {
             Err(Error::WrongHttpResponseCode(actual))
         } else {
@@ -27,7 +41,7 @@ impl GerritRestApi {
         }
     }
 
-    fn json_strip_magic_prefix(content: String) -> Result<String> {
+    fn strip_json_magic_prefix(content: String) -> Result<String> {
         const MAGIC_PREFIX: &'static str = ")]}'\n";
         if !content.starts_with(MAGIC_PREFIX) {
             return Err(Error::NotJsonResponse(content));
