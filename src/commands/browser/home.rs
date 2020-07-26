85992dfd3344520d1695e58e07499f04689025b1
Natanael Rabello <natanael.rabello@outlook.com>
2020-07-26 00:12:54 -0300

    Testing simple UI table
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

---
+++ src/commands/browser/home.rs
@@ -0,0 +1,74 @@
+use crate::commands::browser::util::event::{Event, Events};
+use termion::event::Key;
+use termion::{input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use tui::style::{Modifier, Style};
+use tui::{
+    backend::{Backend, TermionBackend},
+    layout::{Constraint, Layout},
+    widgets::{Block, Borders, Row, Table},
+    Frame, Terminal,
+};
+
+pub fn main() -> Result<(), failure::Error> {
+    let stdout = std::io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let events = Events::new();
+
+    loop {
+        terminal.draw(|frame| draw(frame))?;
+
+        if let Event::Input(key) = events.next()? {
+            match key {
+                Key::Char('q') | Key::Ctrl('c') => {
+                    break;
+                }
+                _ => {}
+            }
+        };
+    }
+
+    Ok(())
+}
+
+fn draw<B>(mut frame: Frame<B>)
+where
+    B: Backend,
+{
+    let windows = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .split(frame.size());
+    let header = ["number", "project", "subject"];
+    let data = [
+        [
+            "96895",
+            "dmos-hal-filters-ll-bcm",
+            "[US92241] Implement ActionMplsPortIgnoreVlanAndStgCheck",
+        ],
+        [
+            "98677",
+            "dmos-hal-switch-vpn-ll-bcm",
+            "[US93084] Also remove encap when deleting all VPNs",
+        ],
+    ];
+    let rows = data.iter().map(|value| Row::Data(value.iter()));
+    let table = Table::new(header.iter(), rows)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title("Outgoing Reviews")
+                .title_style(Style::new().modifier(Modifier::BOLD | Modifier::ITALIC)),
+        )
+        .header_gap(0)
+        .header_style(Style::new().modifier(Modifier::DIM))
+        .widths(&[
+            Constraint::Length(6),
+            Constraint::Length(30),
+            Constraint::Percentage(100),
+        ]);
+    frame.render_widget(table, windows[0]);
+}
--- src/commands/browser/mod.rs
+++ src/commands/browser/mod.rs
@@ -1,6 +1,8 @@
 use crate::config::{CliConfig, Verbosity};
-use clap::{App, AppSettings, ArgMatches, SubCommand};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 
+mod home;
+mod table;
 mod user_input;
 #[allow(dead_code)]
 pub mod util;
@@ -10,15 +12,29 @@ pub fn cli() -> App<'static, 'static> {
         .about("Interactive terminal browser")
         .setting(AppSettings::Hidden)
         .template("{about}\n\nUSAGE:\n    {usage}\n\n{all-args}")
+        .arg(
+            Arg::with_name("mode")
+                .long("mode")
+                .short("m")
+                .help("UI mode [testing]")
+                .possible_values(&["input", "table", "home"])
+                .takes_value(true),
+        )
 }
 
 pub fn exec(_config: &mut CliConfig, args: Option<&ArgMatches>) -> Result<(), failure::Error> {
     let args = args.unwrap();
     let _verbose: Verbosity = args.occurrences_of("verbose").into();
-    browser()
+    let mode = args.value_of("mode").unwrap_or("home");
+    browser(mode)
 }
 
-pub fn browser() -> Result<(), failure::Error> {
-    user_input::main().unwrap();
+pub fn browser(mode: &str) -> Result<(), failure::Error> {
+    match mode {
+        "input" => user_input::main().unwrap(),
+        "table" => table::main().unwrap(),
+        "home" => home::main().unwrap(),
+        &_ => panic!(),
+    }
     Ok(())
 }
